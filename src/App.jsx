311dd0852d8c2e5d1212934313e6f02320e1c8f5
Vipul Kumar <vipulkr0808@gmail.com>
2022-04-16 11:35:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.scss";
+import Home from "./home/Home";
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/home/Home.jsx
@@ -0,0 +1,147 @@
+import Navbar from "../components/navbar/Navbar";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, sint
+        sapiente reiciendis, ut, est ducimus harum porro ratione nostrum culpa
+        consectetur. Molestiae eius eligendi facere earum aperiam et sint esse.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
